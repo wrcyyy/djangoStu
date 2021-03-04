37e0b9e76f5b4841d2ed5723a04c77f6d5a9063e
wrc <wrcyyy@126.com>
2021-03-04 23:08:12 +0800

    update project

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,60 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from .models import Question, Choice
 
 
 # Create your views here.
 
-def index(request):
-    context = {
-        'latest_question_list': Question.objects.order_by('-pub_date')[:5]
-    }
-    return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    return render(request, 'polls/detail.html', {'question': get_object_or_404(Question, pk=question_id)})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    return HttpResponse(f'您正在查看问题结果：{question_id}')
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def index(request):
+#     context = {
+#         'latest_question_list': Question.objects.order_by('-pub_date')[:5]
+#     }
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     return render(request, 'polls/detail.html', {'question': get_object_or_404(Question, pk=question_id)})
+#
+#
+# def results(request, question_id):
+#     return render(request, 'polls/results.html', {'question': get_object_or_404(Question, pk=question_id)})
 
 
 def vote(request, question_id):
-    return HttpResponse(f'您正在给问题{question_id}投票')
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question, 'error_message': '请选择一个选项'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
